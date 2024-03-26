974847343a2833a889ad37f384349a36eeaca7ed
Niels van der Vlist <nielsvandervlist@MacBook-Pro-van-Niels.local>
2024-03-26 09:52:51 +0100

    Add comments

--- src/components/Rounds/FileDrop.js
+++ src/components/Rounds/FileDrop.js
@@ -1,26 +1,26 @@
-import {Fetcher} from 'ra-fetch'
-import {useEffect, useState} from 'react'
-import useAuth from '@/hooks/auth'
-import DragAndDrop from '@/components/Forms/DragDrop'
+// import {Fetcher} from 'ra-fetch'
+// import {useEffect, useState} from 'react'
+// import useAuth from '@/hooks/auth'
+// import DragAndDrop from '@/components/Forms/DragDrop'
 
-function FileDrop({documents, id}){
+// function FileDrop({documents, id}){
 
-    const {user} = useAuth({middleware: 'auth'})
-    const [rounds, setRounds] = useState()
+//     const {user} = useAuth({middleware: 'auth'})
+//     const [rounds, setRounds] = useState()
 
-    //TODO Rounds is not yet used
-    useEffect(() => {
-            if (user?.id)
-                Fetcher.api('backend')
-                    .index('rounds', {
-                        crisis_id: id,
-                    }).then(response => setRounds(response))
-        }, [user?.id],
-    )
+//     //TODO Rounds is not yet used
+//     useEffect(() => {
+//             if (user?.id)
+//                 Fetcher.api('backend')
+//                     .index('rounds', {
+//                         crisis_id: id,
+//                     }).then(response => setRounds(response))
+//         }, [user?.id],
+//     )
 
-    return <div>
-        {rounds && <DragAndDrop data={documents.data} rounds={rounds}/>}
-    </div>
-}
+//     return <div>
+//         {rounds && <DragAndDrop data={documents.data} rounds={rounds}/>}
+//     </div>
+// }
 
-export default FileDrop
+// export default FileDrop
